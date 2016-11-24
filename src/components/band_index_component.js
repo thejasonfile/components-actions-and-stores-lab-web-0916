fd528fa899851ec167a880ee9e15eca75eec9c0d
Jason Arnold <jason.arnold.00@gmail.com>
2016-11-24 10:26:18 -0500

    Done.

---
+++ src/components/band_index_component.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BandIndex = function(props){
+  return(
+    <li>
+      {props.band.title}
+    </li>
+  )
+}
+
+export default BandIndex;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from './store';
+import BandInput from './components/band_input_component';
+import bandReducer from './reducers/band_reducer';
 
-ReactDOM.render(<InsertTopLevelComponent />, document.getElementById('container'))
+const store = createStore(bandReducer);
+
+const renderApp = function(){
+  ReactDOM.render(<BandInput store={store} />, document.getElementById('container'))
+}
+
+store.subscribe(renderApp);
+store.dispatch({})
 
 require('../test/index-test.js'); // Leave this in!
